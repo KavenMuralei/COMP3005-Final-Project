24bd83136b455ad2f38b343930b35b139c92ece2
Kaven Muraleitharan <kavenmuraleitharan0726@gmail.com>
2025-11-26 18:15:15 -0500

    ddl sql updated

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -1,25 +1,44 @@
-CREATE TABLE Users (
-	user_id SERIAL PRIMARY KEY,
-	first_name TEXT NOT NULL,
-	last_name TEXT NOT NULL,
-	email TEXT NOT NULL UNIQUE,
-	password TEXT NOT NULL,
-	date_of_birth DATE NOT NULL,
-	gender TEXT NOT NULL,
-	role TEXT NOT NULL CHECK (role IN ('member', 'trainer', 'admin'))
+
+CREATE TABLE Member (
+    member_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    contact_email VARCHAR(255) UNIQUE NOT NULL,
+    phone_number VARCHAR(20),
+    date_of_birth DATE,
+    gender VARCHAR(255)
 );
 
-CREATE TABLE Members (
-    member_id INT PRIMARY KEY
-        REFERENCES users(user_id) ON DELETE CASCADE
+CREATE TABLE Trainer (
+    trainer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
 );
 
-CREATE TABLE Trainers (
-    trainer_id INT PRIMARY KEY
-        REFERENCES users(user_id) ON DELETE CASCADE
+CREATE TABLE Admin (
+    admin_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    contact_email VARCHAR(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE Admins (
-    admin_id INT PRIMARY KEY
-        REFERENCES users(user_id) ON DELETE CASCADE
+CREATE TABLE Room (
+    room_id SERIAL PRIMARY KEY,
+    room_name VARCHAR(255) UNIQUE NOT NULL,
+    capacity INT CHECK (capacity > 0),
+    location_details VARCHAR(255)
 );
+
+CREATE TABLE Equipment (
+    equipment_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    status VARCHAR(50) NOT NULL DEFAULT 'OK'
+);
+
+CREATE TABLE Class (
+    class_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    description TEXT,
+);
+
--- sql/DML.sql
+++ sql/DML.sql
@@ -1,12 +1,12 @@
-INSERT INTO Users (first_name, last_name, email, password, date_of_birth, gender, role)
-VALUES
-('kaven', 'mura', 'kavenmura@gmail.com', 'pass', '2005-07-26', 'male', 'admin'),
-('test1', 'test11', 'test1@test.com', 'test1pass', '1900-01-01', 'female', 'trainer'),
-('test2', 'test22', 'test2@test.com', 'test2pass', '1900-01-01', 'other', 'member');
+-- INSERT INTO Users (first_name, last_name, email, password, date_of_birth, gender, role)
+-- VALUES
+-- ('kaven', 'mura', 'kavenmura@gmail.com', 'pass', '2005-07-26', 'male', 'admin'),
+-- ('test1', 'test11', 'test1@test.com', 'test1pass', '1900-01-01', 'female', 'trainer'),
+-- ('test2', 'test22', 'test2@test.com', 'test2pass', '1900-01-01', 'other', 'member');
 
 
-INSERT INTO Members (member_id) VALUES (1);
-INSERT INTO Trainers (trainer_id) VALUES (2);
-INSERT INTO Admins (admin_id) VALUES (3);
+-- INSERT INTO Members (member_id) VALUES (1);
+-- INSERT INTO Trainers (trainer_id) VALUES (2);
+-- INSERT INTO Admins (admin_id) VALUES (3);
 
-SELECT * FROM Users;
+-- SELECT * FROM Users;
