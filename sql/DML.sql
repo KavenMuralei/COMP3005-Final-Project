8f368a74486b60175e54d829d5a20bfe24b678c1
Dean Sabbah <deanreynolds10@yahoo.ca>
2025-12-01 20:35:48 -0500

    added default users

--- sql/DML.sql
+++ sql/DML.sql
@@ -18,3 +18,35 @@ WITH new_user AS (
 )
 INSERT INTO Member(member_id, phone_number, date_of_birth, gender)
 SELECT user_id, '123-456-7890', '2002-10-07', 'male' FROM new_user;
+
+WITH new_user AS (
+  INSERT INTO "User"(first_name, last_name, email, user_password, user_type)
+  VALUES ('Bill','Nye','billnye@science.guy', 'password', 0)
+  RETURNING user_id
+)
+INSERT INTO Member(member_id, phone_number, date_of_birth, gender)
+SELECT user_id, '123-456-7890', '1955-11-27', 'male' FROM new_user;
+
+WITH new_user AS (
+  INSERT INTO "User"(first_name, last_name, email, user_password, user_type)
+  VALUES ('April','Suung','april.suung@email.com', 'password', 0)
+  RETURNING user_id
+)
+INSERT INTO Member(member_id, phone_number, date_of_birth, gender)
+SELECT user_id, '123-456-7890', '1998-02-16', 'female' FROM new_user;
+
+WITH new_user AS (
+  INSERT INTO "User"(first_name, last_name, email, user_password, user_type)
+  VALUES ('Ansh','Sharma','anshsharma@cmail.carleton.ca', 'password', 1)
+  RETURNING user_id
+)
+INSERT INTO Trainer(trainer_id)
+SELECT user_id FROM new_user;
+
+WITH new_user AS (
+  INSERT INTO "User"(first_name, last_name, email, user_password, user_type)
+  VALUES ('Kaven','Muraleitharan','kavenmuraleitharan@cmail.carleton.ca', 'password', 2)
+  RETURNING user_id
+)
+INSERT INTO Admin(admin_id)
+SELECT user_id FROM new_user;
