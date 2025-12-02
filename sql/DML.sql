323d5aaf91f3edd53c6df36d1fd98effbefc590d
Kaven Muraleitharan <kavenmuraleitharan0726@gmail.com>
2025-12-02 17:47:54 -0500

    added more equpment to dml.sql

--- sql/DML.sql
+++ sql/DML.sql
@@ -1,16 +1,3 @@
--- INSERT INTO Users (first_name, last_name, email, password, date_of_birth, gender, role)
--- VALUES
--- ('kaven', 'mura', 'kavenmura@gmail.com', 'pass', '2005-07-26', 'male', 'admin'),
--- ('test1', 'test11', 'test1@test.com', 'test1pass', '1900-01-01', 'female', 'trainer'),
--- ('test2', 'test22', 'test2@test.com', 'test2pass', '1900-01-01', 'other', 'member');
-
-
--- INSERT INTO Members (member_id) VALUES (1);
--- INSERT INTO Trainers (trainer_id) VALUES (2);
--- INSERT INTO Admins (admin_id) VALUES (3);
-
--- SELECT * FROM Users;
-
 WITH new_user AS (
   INSERT INTO "User"(first_name, last_name, email, user_password, user_type)
   VALUES ('Dean','Sabbah','deansabbah@cmail.carleton.ca', 'password', 0)
