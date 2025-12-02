038eff182ce85c2f7257ab72a2a1019b94161ea9
Dean Sabbah <deanreynolds10@yahoo.ca>
2025-12-01 21:23:36 -0500

    fixed room check

--- app/src/main/java/Room.java
+++ app/src/main/java/Room.java
@@ -8,34 +8,31 @@ import java.util.Scanner;
 public class Room {
     public static boolean available(int room_id, Date day, Time start_time, Time end_time, Connection connection, Scanner input) {
         String query = """
-                SELECT day, start_time, end_time
-                FROM Room
-                WHERE room_id = ?
+                SELECT start_time, end_time
+                FROM Booking
+                WHERE room_id = ? AND day = ?
                 """;
         
         try (PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setInt(1, room_id);
+            ps.setDate(2, day);
             try (ResultSet rs = ps.executeQuery()){
-                if(!rs.next()) {
-                    System.out.println("Room not found");
-                    return false;
-                }
-
                 while(rs.next()) {
-                    Date rm_day = rs.getDate("day");
                     Time rm_starTime = rs.getTime("start_time");
                     Time rm_endtime = rs.getTime("end_time");
 
-                    if((start_time.after(rm_starTime) && end_time.before(rm_endtime)) || (start_time.before(rm_endtime) && end_time.after(rm_starTime)) && day.equals(rm_day))
+                    if(start_time.before(rm_endtime) && end_time.after(rm_starTime))
                         return false;
                 }
             } catch (Exception e) {
                 System.err.println("Error finding room:");
                 System.err.println(e);
+                return false;
             }
         } catch (Exception e) {
             System.err.println("Error connecting to database:");
             System.err.println(e);
+            return false;
         }
 
         return true;
